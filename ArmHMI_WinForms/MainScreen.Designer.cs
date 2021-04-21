8df30c3586ab73f61e093ec252c2fc045dd55829
Dave <hobbesdcc@gmail.com>
2021-04-21 13:35:08 -0400

    Working serial interface for HMI form program

--- ArmHMI_WinForms/MainScreen.Designer.cs
+++ ArmHMI_WinForms/MainScreen.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.bnt_Home_All = new System.Windows.Forms.Button();
 			this.bnt_Home_AxisA = new System.Windows.Forms.Button();
 			this.bnt_Home_AxisB = new System.Windows.Forms.Button();
@@ -40,11 +41,17 @@
 			this.OutputText_PortName = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.OutputText_BaudRate = new System.Windows.Forms.TextBox();
-			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.Bnt_Serial_StartListen = new System.Windows.Forms.Button();
 			this.Bnt_Serial_StopListen = new System.Windows.Forms.Button();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.richTextBox_textReceiver = new System.Windows.Forms.RichTextBox();
+			this.label_status = new System.Windows.Forms.Label();
+			this.textBox_textSent = new System.Windows.Forms.TextBox();
+			this.Bnt_Serial_Send = new System.Windows.Forms.Button();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
 			this.groupBox1.SuspendLayout();
 			this.groupBox4.SuspendLayout();
+			this.groupBox5.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// bnt_Home_All
@@ -104,29 +111,30 @@
 			// 
 			this.groupBox2.Location = new System.Drawing.Point(15, 12);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(170, 100);
+			this.groupBox2.Size = new System.Drawing.Size(442, 100);
 			this.groupBox2.TabIndex = 8;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Mode Selection";
 			// 
 			// groupBox3
 			// 
-			this.groupBox3.Location = new System.Drawing.Point(257, 12);
+			this.groupBox3.Location = new System.Drawing.Point(191, 118);
 			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(200, 184);
+			this.groupBox3.Size = new System.Drawing.Size(266, 220);
 			this.groupBox3.TabIndex = 9;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "Manual Move";
 			// 
 			// groupBox4
 			// 
+			this.groupBox4.BackColor = System.Drawing.SystemColors.ControlDark;
 			this.groupBox4.Controls.Add(this.label2);
 			this.groupBox4.Controls.Add(this.OutputText_PortName);
 			this.groupBox4.Controls.Add(this.label1);
 			this.groupBox4.Controls.Add(this.OutputText_BaudRate);
-			this.groupBox4.Location = new System.Drawing.Point(644, 12);
+			this.groupBox4.Location = new System.Drawing.Point(319, 19);
 			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Size = new System.Drawing.Size(144, 78);
+			this.groupBox4.Size = new System.Drawing.Size(144, 77);
 			this.groupBox4.TabIndex = 10;
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "Connection Info";
@@ -165,18 +173,9 @@
 			this.OutputText_BaudRate.TabIndex = 3;
 			this.OutputText_BaudRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(380, 250);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(303, 158);
-			this.textBox1.TabIndex = 11;
-			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-			// 
 			// Bnt_Serial_StartListen
 			// 
-			this.Bnt_Serial_StartListen.Location = new System.Drawing.Point(380, 214);
+			this.Bnt_Serial_StartListen.Location = new System.Drawing.Point(13, 27);
 			this.Bnt_Serial_StartListen.Name = "Bnt_Serial_StartListen";
 			this.Bnt_Serial_StartListen.Size = new System.Drawing.Size(150, 30);
 			this.Bnt_Serial_StartListen.TabIndex = 12;
@@ -186,7 +185,7 @@
 			// 
 			// Bnt_Serial_StopListen
 			// 
-			this.Bnt_Serial_StopListen.Location = new System.Drawing.Point(533, 214);
+			this.Bnt_Serial_StopListen.Location = new System.Drawing.Point(13, 63);
 			this.Bnt_Serial_StopListen.Name = "Bnt_Serial_StopListen";
 			this.Bnt_Serial_StopListen.Size = new System.Drawing.Size(150, 30);
 			this.Bnt_Serial_StopListen.TabIndex = 13;
@@ -194,33 +193,90 @@
 			this.Bnt_Serial_StopListen.UseVisualStyleBackColor = true;
 			this.Bnt_Serial_StopListen.Click += new System.EventHandler(this.Bnt_Serial_StopListen_Click);
 			// 
+			// serialPort1
+			// 
+			this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+			// 
+			// richTextBox_textReceiver
+			// 
+			this.richTextBox_textReceiver.Location = new System.Drawing.Point(13, 140);
+			this.richTextBox_textReceiver.Name = "richTextBox_textReceiver";
+			this.richTextBox_textReceiver.Size = new System.Drawing.Size(450, 250);
+			this.richTextBox_textReceiver.TabIndex = 14;
+			this.richTextBox_textReceiver.Text = "";
+			this.richTextBox_textReceiver.TextChanged += new System.EventHandler(this.richTextBox_textReceiver_TextChanged);
+			// 
+			// label_status
+			// 
+			this.label_status.BackColor = System.Drawing.SystemColors.Window;
+			this.label_status.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.label_status.Location = new System.Drawing.Point(238, 152);
+			this.label_status.Name = "label_status";
+			this.label_status.Size = new System.Drawing.Size(200, 13);
+			this.label_status.TabIndex = 15;
+			this.label_status.Text = "DISCONNECTED";
+			this.label_status.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBox_textSent
+			// 
+			this.textBox_textSent.Location = new System.Drawing.Point(13, 111);
+			this.textBox_textSent.Name = "textBox_textSent";
+			this.textBox_textSent.Size = new System.Drawing.Size(369, 20);
+			this.textBox_textSent.TabIndex = 16;
+			// 
+			// Bnt_Serial_Send
+			// 
+			this.Bnt_Serial_Send.Location = new System.Drawing.Point(388, 108);
+			this.Bnt_Serial_Send.Name = "Bnt_Serial_Send";
+			this.Bnt_Serial_Send.Size = new System.Drawing.Size(75, 25);
+			this.Bnt_Serial_Send.TabIndex = 17;
+			this.Bnt_Serial_Send.Text = "Send";
+			this.Bnt_Serial_Send.UseVisualStyleBackColor = true;
+			this.Bnt_Serial_Send.Click += new System.EventHandler(this.Bnt_Serial_Send_Click);
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Controls.Add(this.textBox_textSent);
+			this.groupBox5.Controls.Add(this.groupBox4);
+			this.groupBox5.Controls.Add(this.Bnt_Serial_Send);
+			this.groupBox5.Controls.Add(this.Bnt_Serial_StartListen);
+			this.groupBox5.Controls.Add(this.Bnt_Serial_StopListen);
+			this.groupBox5.Controls.Add(this.label_status);
+			this.groupBox5.Controls.Add(this.richTextBox_textReceiver);
+			this.groupBox5.Location = new System.Drawing.Point(463, 14);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(481, 416);
+			this.groupBox5.TabIndex = 18;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Serial Listener";
+			// 
 			// MainScreen
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.WindowFrame;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.Bnt_Serial_StopListen);
-			this.Controls.Add(this.Bnt_Serial_StartListen);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.groupBox4);
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ClientSize = new System.Drawing.Size(973, 475);
+			this.Controls.Add(this.groupBox5);
 			this.Controls.Add(this.groupBox3);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Name = "MainScreen";
 			this.Text = "Robot Arm HMI";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainScreen_FormClosing);
+			this.Load += new System.EventHandler(this.MainScreen_Load);
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox4.ResumeLayout(false);
 			this.groupBox4.PerformLayout();
+			this.groupBox5.ResumeLayout(false);
+			this.groupBox5.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button bnt_Home_All;
+		public System.Windows.Forms.Button bnt_Home_All;
 		private System.Windows.Forms.Button bnt_Home_AxisA;
 		private System.Windows.Forms.Button bnt_Home_AxisB;
 		private System.Windows.Forms.Button bnt_Home_Base;
@@ -232,9 +288,14 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox OutputText_PortName;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button Bnt_Serial_StartListen;
 		private System.Windows.Forms.Button Bnt_Serial_StopListen;
+		private System.IO.Ports.SerialPort serialPort1;
+		private System.Windows.Forms.RichTextBox richTextBox_textReceiver;
+		private System.Windows.Forms.Label label_status;
+		private System.Windows.Forms.TextBox textBox_textSent;
+		private System.Windows.Forms.Button Bnt_Serial_Send;
+		private System.Windows.Forms.GroupBox groupBox5;
 	}
 }
 
--- ArmHMI_WinForms/MainScreen.cs
+++ ArmHMI_WinForms/MainScreen.cs
@@ -13,9 +13,7 @@ namespace ArmHMI_WinForms
 {
 	public partial class MainScreen : Form
 	{
-
-		private SerialPort mySerialPort;
-		bool serialConnected;
+		string serialDataIn;
 
 
 		public MainScreen()
@@ -23,64 +21,36 @@ namespace ArmHMI_WinForms
 			InitializeComponent();
 		}
 
-
-		private void SerialSetup()
+		private void MainScreen_Load(object sender, EventArgs e)
 		{
-			String data;
-			String inputvalue;
+			//add defult settings
+			Bnt_Serial_StartListen.Enabled = true;
+			Bnt_Serial_StopListen.Enabled = false;
+			label_status.Text = "DISCONNECTED";
+			label_status.ForeColor = Color.Red;
+		}
 
-			mySerialPort = new SerialPort();
-			mySerialPort.BaudRate = 9600;
-			mySerialPort.PortName = "COM5";
-			//mySerialport.ReadTimeout = 1000;
-			//mySerialport.WriteTimeout = 1000;
-			
-			mySerialPort.Open();
-
-			//Output info to HMI screen
-			OutputText_BaudRate.Text = mySerialPort.BaudRate.ToString();
-			OutputText_PortName.Text = mySerialPort.PortName.ToString();
-			
-			serialConnected = true;
-			mySerialPort.WriteLine("\r\n");
-
-			while (serialConnected)
+		private void MainScreen_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			//On form close, close open serial port
+			if (serialPort1.IsOpen)
 			{
-				data = mySerialPort.ReadLine();
-				Console.WriteLine(data);
-
-
-				if (data == "Input GOTO X: \r" || data == "Input GOTO Y: \r")
+				try
 				{
-					//inputvalue = Console.ReadLine();
-					//mySerialPort.WriteLine(inputvalue);
-					Bnt_Home_All_Click.PerformClick();
+					serialPort1.Close();
+				}
+				catch (Exception error)
+				{
+					MessageBox.Show(error.Message);
 				}
 			}
 		}
 
-		private void SerialStop()
-		{
-			mySerialPort.Close();
-
-			//Output info to HMI screen
-			OutputText_BaudRate.Text = "";
-			OutputText_PortName.Text = "";
-		}
-
-
-		void sendSerialCommand()//String command
-		{
-			
-		}
-
-
-		
 
 		//Homing Buttons  --------------------------------------------------------------------------------------- 
 		private void Bnt_Home_All_Click(object sender, EventArgs e)
 		{
-			mySerialPort.WriteLine("10");
+			serialPort1.WriteLine("10");
 		}
 
 		private void Bnt_Home_AxisA_Click(object sender, EventArgs e)
@@ -94,24 +64,98 @@ namespace ArmHMI_WinForms
 		}
 
 		private void Bnt_Home_Base_Click(object sender, EventArgs e)
-		{
-			//mySerialport.WriteLine("\r\n");
-			textBox1.Text = textBox1.Text + mySerialPort.ReadLine() + Environment.NewLine;
+		{   /*
+			//serialPort1.WriteLine("\r\n");
+			textBox1.Text = textBox1.Text + serialPort1.ReadLine() + Environment.NewLine;
 			textBox1.SelectionStart = textBox1.TextLength;
 			textBox1.ScrollToCaret();
+			//*/
 		}
 
 		// Serial Listen Buttons --------------------------------------------------------------------------------------- 
 		private void Bnt_Serial_StartListen_Click(object sender, EventArgs e)
 		{
-			SerialSetup();
+			try
+			{
+				//Open Serial Port
+				serialPort1.BaudRate = 9600;
+				serialPort1.PortName = "COM5";
+				serialPort1.Open();
+
+				//GUI Output info about comms
+				OutputText_BaudRate.Text = serialPort1.BaudRate.ToString();
+				OutputText_PortName.Text = serialPort1.PortName.ToString();
+
+				//GUI status
+				Bnt_Serial_StartListen.Enabled = false;
+				Bnt_Serial_StopListen.Enabled = true;
+				label_status.Text = "CONNECTED";
+				label_status.ForeColor = Color.Green;
+			}
+			catch (Exception error)
+			{
+				MessageBox.Show(error.Message);
+				label_status.Text = "FAILED TO CONNECT!";
+			}
+
 		}
 
 		private void Bnt_Serial_StopListen_Click(object sender, EventArgs e)
 		{
-			SerialStop();
+			//Check if port is open, if so try to close, and update UI
+			if (serialPort1.IsOpen)
+			{
+				try
+				{
+					serialPort1.Close();
+
+					//Output info to HMI screen
+					OutputText_BaudRate.Text = "";
+					OutputText_PortName.Text = "";
+					Bnt_Serial_StartListen.Enabled = true;
+					Bnt_Serial_StopListen.Enabled = false;
+					label_status.Text = "DISCONNECTED";
+					label_status.ForeColor = Color.Red;
+
+				}
+				catch (Exception error)
+				{
+					MessageBox.Show(error.Message);
+					label_status.Text = "FAILED TO CLOSE PORT!";
+				}
+			}
 		}
 
+		private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			Console.WriteLine("HEY");
+			serialDataIn = serialPort1.ReadExisting();
+			this.Invoke(new EventHandler(ShowData));
+			Console.WriteLine(serialDataIn);
+		}
 
+		private void ShowData(object sender, EventArgs e)
+		{
+			richTextBox_textReceiver.Text += serialDataIn;
+		}
+
+		private void richTextBox_textReceiver_TextChanged(object sender, EventArgs e)
+		{
+			richTextBox_textReceiver.SelectionStart = richTextBox_textReceiver.Text.Length;
+			richTextBox_textReceiver.ScrollToCaret();
+		}
+
+		private void Bnt_Serial_Send_Click(object sender, EventArgs e)
+		{
+			//Try to send what ever is in "textBox_textSent" on click
+			try
+			{
+				serialPort1.Write(textBox_textSent.Text + "#");
+			}
+			catch (Exception error)
+			{
+				MessageBox.Show(error.Message);
+			}
+		}
 	}
 }
